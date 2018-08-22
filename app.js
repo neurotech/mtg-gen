1e628378ac8d7cdaac4c2564a633691475e96008
Tim Douglas <tim.douglas@phocassoftware.com>
2018-08-23 08:01:29 +1000

    Make it so.

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const brain = require("brain.js");
+const tiny = require("tiny-json-http");
+
+const uri = "https://api.magicthegathering.io/v1/";
+const cardsPath = "cards?page=";
+
+let cards = [];
+let i = 1;
+
+const getCards = i => {
+  tiny.get({ url: uri + cardsPath + i }, (err, res) => {
+    if (err) return err;
+
+    if (res.body.cards.length > 0) {
+      res.body.cards.forEach(element => {
+        cards.push({
+          name: element.name,
+          flavor: element.flavor,
+          artist: element.artist
+        });
+      });
+      console.log(`Captured page ${i}.`);
+      getCards(i + 1);
+    } else {
+      fs.writeFileSync("./cards.json", JSON.stringify(cards, null, "  "));
+      console.log(`Captured all pages.`);
+      console.log(`Size of collection: ${cards.length}`);
+    }
+  });
+};
+
+getCards(i);
